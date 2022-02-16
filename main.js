0e9972fd28d2ac191bd6845f2c757bf935ccc16d
Priscilla Silva <priscilla.sg91@gmail.com>
2022-02-16 13:18:59 +0100

    works but doest at the same time

--- main.js
+++ main.js
@@ -4,36 +4,42 @@
         max = Math.floor(10)
         return Math.floor(Math.random() * (max - min) + min);
     }
+   
     let result = getRandomIntInclusive(0,10);
     let randomNum = result;
-    let userInput = parseInt(prompt("Enter a numbeer between 0 and 10"));
+    console.log(result);
 
-    // prompt("Enter a number between 0 and 10"); {
-    while (true) {
-        if (userInput > randomNum) {
-            parseInt(prompt("Less! Try again"))
-            break
-        } else if (userInput < randomNum) {
-            parseInt(prompt("More! Try again"))
-            break
-        } else if (userInput === randomNum) {
-            alert('Correct!')
-            break
-        }
-        
+    let userInput = parseInt(prompt("Choose a numbeer between 0 and 10"));
+    
+    while (userInput > randomNum) {
+        parseInt(prompt("Smaller! Try again."));
+        randomNum++
     }
+    while (userInput < randomNum) {
+        parseInt(prompt("Bigger! Try again."));
+        randomNum--
+    }
+    while (userInput === randomNum) {
+        alert('Correct!');
+            break;
+    }
+    
+    // while(true) {
+    
+    //     if (userInput > randomNum) {
+    //         parseInt(prompt("Smaller! Try again."));
+    //             break;
+    //     } else if (userInput < randomNum) {
+    //         parseInt(prompt("Bigger! Try again."));
+    //             break;
+    //     } else if (userInput === randomNum) {
+    //         alert("Correct!");
+    //             break;
+    //     }
+    // }
 
-    // do {
-    //     alert('Less!')
-    //     parseInt(prompt("Enter a numbeer between 0 and 10"))
-    // } while ;
-    // do {
-    //     alert('More!')
-    //     parseInt(prompt("Enter a numbeer between 0 and 10"))
-    // } while (userInput < randomNum);
-    // do {
-    //     alert('Correct!')
-    // } while (userInput === randomNum);
+
+
 
 
 
